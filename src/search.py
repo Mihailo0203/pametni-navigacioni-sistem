174813189e99e008ad0296a360d9c53023f579fd
Mihailo0203 <mihailodjeric05@gmail.com>
2026-06-15 17:54:46 +0200

    Dodat IDA* i Greedy algoritam

--- src/search.py
+++ src/search.py
@@ -93,3 +93,62 @@ class AStarSearch(Search):
         min_state = min(states, key=lambda x: x.get_current_cost() + x.get_cost_estimate())
         states.remove(min_state)
         return min_state
+    
+class IterativeDeepeningAStar(Search):
+    def select_state(self, states):
+        pass # Ову методу не користимо јер мењамо целу search логику
+
+    def search(self, initial_state):
+        # Почетна граница (bound) је хеуристика почетног чвора
+        bound = initial_state.get_current_cost() + initial_state.get_cost_estimate()
+        processed_list = [] # Листа за метрику (просторна сложеност)
+        
+        while True:
+            # Стек за Итеративни DFS: чува тупл (стање, сет_посећених_id_јева_на_путањи)
+            # Сет користимо да спречимо алгоритам да се врти у круг у истој улици
+            stack = [(initial_state, {initial_state.unique_hash()})]
+            
+            min_exceeded = float('inf')
+            found_goal = None
+            
+            while stack:
+                curr_state, current_path_set = stack.pop()
+                processed_list.append(curr_state)
+                
+                f = curr_state.get_current_cost() + curr_state.get_cost_estimate()
+                
+                # Ако тренутна цена премашује границу, бележимо је за следећу итерацију и сечемо грану
+                if f > bound:
+                    if f < min_exceeded:
+                        min_exceeded = f
+                    continue
+                    
+                if curr_state.is_final_state():
+                    found_goal = curr_state
+                    break
+                    
+                # Додавање следећих стања на стек
+                for next_state in curr_state.get_next_states():
+                    if next_state.unique_hash() not in current_path_set:
+                        # Правимо копију сета за нову грану претраге
+                        new_path_set = set(current_path_set)
+                        new_path_set.add(next_state.unique_hash())
+                        stack.append((next_state, new_path_set))
+                        
+            # Ако смо нашли циљ, враћамо путању
+            if found_goal is not None:
+                return Search.reconstruct_path(found_goal), processed_list, []
+                
+            # Ако нема више чворова за обилазак, путања не постоји
+            if min_exceeded == float('inf'):
+                return None, processed_list, []
+                
+            # Повећавамо границу за следећу итерацију
+            bound = min_exceeded
+
+class GreedySearch(Search):
+    def select_state(self, states):
+        # Pohlepna pretraga gleda SAMO heuristiku h(n), ignoriše g(n)
+        min_state = min(states, key=lambda x: x.get_cost_estimate())
+        states.remove(min_state)
+        return min_state
